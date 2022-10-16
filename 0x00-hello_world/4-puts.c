ed698873dbdbf3d47a1015813c3d48445a263770
samchizoba <lordslove0001@gmail.com>
2022-10-16 13:12:40 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main is the entry point
- * i used the function puts to inser my code.
- * the description is Programming is like building a multilingual puzzle
- * my return was o
+ *main- print a line of code using puts
+ *return 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
