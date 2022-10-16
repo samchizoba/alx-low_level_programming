b6612c999db4b9a1f70377bca3536e01b47d0d12
samchizoba <lordslove0001@gmail.com>
2022-10-16 07:12:07 -0700

    fouth project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main is the entry point
+ * i used the function puts to inser my code.
+ * the description is Programming is like building a multilingual puzzle
+ * my return was o
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
