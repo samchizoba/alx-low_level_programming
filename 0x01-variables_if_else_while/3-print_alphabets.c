916746c98d82bba76c62e286af299f4ee01de73d
samchizoba <lordslove0001@gmail.com>
2022-10-17 15:09:27 -0700

    task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char aph;
+
+	for (aph = 'a'; aph <= 'z'; aph++)
+	{
+		putchar(aph);
+	}
+	for (aph = 'A'; aph <= 'Z'; aph++)
+	{
+		putchar(aph);
+	}
+	putchar('\n');
+	return (0);
+}
